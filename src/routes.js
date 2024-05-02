9edd0841af133eae1580a56f450e5e05331751c4
leleo-arch <leo_xdplay@hotmail.com>
2024-05-01 23:00:45 -0300

    commit -App-2

--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,7 @@ import cadastro from "./containers/Cadastro-Alunos";
 import alunos from "./containers/Alunos";
 import historico  from "./containers/Treino";
 import login  from "./containers/Desafios";
-import menu  from "./containers/Menu";
+import Home  from "./containers/Home";
 import check  from "./containers/Checkin-aula";
 
 function Routes() {
@@ -14,11 +14,11 @@ function Routes() {
   
    <Router>
     <Switch>
+        <Route path="/" exact component={Home} /> {Home}
         <Route exact path="/Treino"component = {historico}/>
         <Route exact path="/Desafios"component = {login}/>
-        <Route exact path="/" component = {cadastro}/>
+        <Route exact path="/Cadastro-Alunos" component = {cadastro}/>
         <Route exact path="/Alunos"component = {alunos}/>
-        <Route exact path="/Menu"component = {menu}/>
         <Route exact path="/Checkin-aula"component = {check}/>
         
     </Switch>
