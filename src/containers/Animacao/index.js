6f7db6f670a8d6289bcc57ddca684e12ec5a6cbb
leleo-arch <leo_xdplay@hotmail.com>
2024-12-06 12:01:55 -0300

    .

--- src/containers/Animacao/index.js
+++ src/containers/Animacao/index.js
@@ -7,7 +7,7 @@ import backgroundImg from '../../assets/5.png';  // Imagem de fundo
 // Animação para o texto
 const fadeIn = keyframes`
   from {
-    opacity: 0;
+    opacity: 1;
   }
   to {
     opacity: 1;
